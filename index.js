0249ab1e6a34d6af68f53518c99995092cd69b1f
Abdihafith <abdyhafith@gmail.com>
2024-03-29 14:57:21 +0300

    complete

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
 const titleCased = () => {
-  return tutorials
-}
+  return tutorials.map((tutorial) => {
+    const words = tutorial.split(' ');
+    const titleCaseWords = words.map(
+      (word) => word.charAt(0).toUpperCase() + word.slice(1)
+    );
+    return titleCaseWords.join(' ');
+  });
+};
+const titleCasedTutorials = titleCased();
+console.log(titleCasedTutorials);
